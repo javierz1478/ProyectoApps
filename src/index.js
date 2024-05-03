567265528b6d5ef5b8cfc2df1afa75eb84480093
jzamora <javier.zamorab@sansano.usm.cl>
2024-05-03 16:05:21 -0400

    Inicialización proyecto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Koa from 'koa'
+import bodyParser from 'koa-body'
+
+import router from './router/index'
+
+const app = new Koa()
+
+const port = 3000
+app.use(bodyParser({ multipart: true, urlencoded: true }))
+
+app.use(router.routes())
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Router from 'koa-router'
+import getHealth from './health/health'
+
+
+const router = new Router()
+
+router.get('/health', getHealth)
+
+
+
+
+export default router
